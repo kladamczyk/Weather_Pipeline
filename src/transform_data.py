5c6a1fc1bc256f9caa1e06c8d45bc3a9dc9aba81
bezwymowki <klaudia.adamczyk707@gmail.com>
2026-04-02 00:51:05 +0200

    Finished logic for function transforming data; exports transformed data to cv file

--- src/transform_data.py
+++ src/transform_data.py
@@ -8,19 +8,36 @@ def read_file(file):
     return data
 
 #ERROR HANDLING!!!
-def transform_data(data):
-    df = pd.DataFrame(data["daily"])
-    print(df)
-    #zmien nazwy kolumn tutaj
+def transform_data(data) -> pd.DataFrame:
+    try:
+        df = pd.DataFrame(data["daily"])
+        df.columns = [
+                    "date", "weather_code", "max_temperature", "min_temperature", "max_apparent_temperature",
+                    "min_apparent_temperature", "sunset_time", "sunrise_time", "rain_sum", "snowfall_sum"
+                    ]
 
-    #zmien typ danych na poprawne
-    df["time"] = pd.to_datetime(df["time"])
+        df.fillna(method='pad', inplace=True)
+        df.dropna(inplace=True)
+        df = df.reset_index(drop=True)
 
-    #ogarnij case jakby byly missing values
+        df["date"] = pd.to_datetime(df["date"])
+        df["weather_code"] = df["weather_code"].astype(int)
+        df["sunset_time"] = pd.to_datetime(df["sunset_time"]).dt.time
+        df["sunrise_time"] = pd.to_datetime(df["sunrise_time"]).dt.time
 
-    print(df.isnull().sum()) #sprawdza, czy jakiekolwiek wartosci sa null
-    #print(df.columns)
+        return df
+    except:
+        print("Boo hoo")
 
-r = read_file('weather_data.json')
-result = transform_data(r)
-#print(result)
+def main():
+    data_weather_forecast = read_file('weather_forecast_data.json')
+    data_weather_past = read_file('weather_past_data.json')
+
+    transformed_weather_forecast = transform_data(data_weather_forecast)
+    transformed_weather_past = transform_data(data_weather_past)
+
+    transformed_weather_forecast.to_csv("transformed_weather_forecast_data.csv", index=False)
+    transformed_weather_past.to_csv("transformed_weather_past_data.csv", index=False)
+
+if __name__ == "__main__":
+    main()
