b538000fa978d7d8bda7805fc60e4c11c356f5f1
admczyk <klaudia.adamczyk707@gmail.com>
2026-04-02 18:13:30 +0000

    Added variables from config file and fixed minor issues

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from config import WeatherConfiguration
 
 def fetch_past_weather_data(latitude, longitude, past_days) -> dict:
     url = (
@@ -23,34 +24,15 @@ def fetch_past_weather_data(latitude, longitude, past_days) -> dict:
     except Exception as e:
         print(f"Unexpected {e}")
 
-def fetch_weather_forecast_data(latitude, longitude, forecast_days) -> dict:
-    url = (
-        "https://api.open-meteo.com/v1/forecast"
-        f"?latitude={latitude}"
-        f"&longitude={longitude}"
-        "&daily="
-            "weather_code,"
-            "temperature_2m_max,temperature_2m_min,"
-            "apparent_temperature_max,apparent_temperature_min,"
-            "rain_sum,snowfall_sum"
-        f"&forecast_days={forecast_days}"
-        )
-    
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-
-        weather_data = response.json()
-        return weather_data
-    except Exception as e:
-        print(f"Unexpected {e}")
-
 def main():
+    city = WeatherConfiguration.DEFAULT_CITY
+    latitude = WeatherConfiguration.DEFAULT_LATITUDE
+    longitude = WeatherConfiguration.DEFAULT_LATITUDE
+    past_days = WeatherConfiguration.DEFAULT_PAST_DAYS
     with open("weather_past_data.json", "w") as file:
-        json.dump(fetch_past_weather_data(52.13, 21.00, 92), file, indent=4)
-
-    with open("weather_forecast_data.json", "w") as file:
-        json.dump(fetch_weather_forecast_data(52.13, 21.00, 16), file, indent=4)
+        json.dump(fetch_past_weather_data(latitude, longitude, past_days), file, indent=4)
+        print(f"Succesfully aquired past weather data in {city}.")
+    file.close()
 
 if __name__ == "__main__":
     main()
