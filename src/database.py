3e6abc57168c2eab5fa8536c740856c58d2c2709
Klaudia <klaudia.adamczyk707@gmail.com>
2026-06-16 07:12:56 +0200

    Update database.py

--- src/database.py
+++ src/database.py
@@ -18,28 +18,22 @@ class WeatherDatabase(Base):
     snowfall_sum = Column(Float, nullable=False)
 
 def create_database():
-    try:
         engine = create_engine(f"{WeatherConfiguration.DATABASE_URL}")
         Base.metadata.create_all(engine)
         return engine
-    except Exception as e:
-        print(f"Unexpected {e}")
 
 def load_data_to_database(df):
     engine = create_database()
 
-    try:
-        df["date"] = pd.to_datetime(df["date"])
+    df["date"] = pd.to_datetime(df["date"])
 
-        existing_dates = pd.read_sql("SELECT date FROM weather_database", Session(engine).bind)
-        existing_dates_set = set(existing_dates["date"])
+    existing_dates = pd.read_sql("SELECT date FROM weather_database", Session(engine).bind)
+    existing_dates_set = set(existing_dates["date"])
 
-        new_df = df[~df["date"].isin(existing_dates_set)]
-        if not new_df.empty:
-            new_df.to_sql("weather_database", engine, if_exists="append", index=False)
-        return True
-    except Exception as e:
-        print(f"Unexpected {e}")
+    new_df = df[~df["date"].isin(existing_dates_set)]
+    if not new_df.empty:
+        new_df.to_sql("weather_database", engine, if_exists="append", index=False)
+    return True
 
 def main():
     df = pd.read_csv("transformed_weather_data.csv")
