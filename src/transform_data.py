c8a15eee0671fe43848e7ce914bfee5c3e3e2b46
Klaudia <klaudia.adamczyk707@gmail.com>
2026-06-16 07:12:18 +0200

    Update transform_data.py

--- src/transform_data.py
+++ src/transform_data.py
@@ -2,32 +2,29 @@ import pandas as pd
 import json
 
 def read_file(file):
-    try:
-        with open(file, "r") as file:
-            data = json.load(file)
-            print(type(data))
-        return data
-    except Exception as e:
-        print(f"Unexpected {e}")
+    """
+    Function thats reads JSON file and returns it.
+    """
+    with open(file, "r") as file:
+        data = json.load(file)
+        print(type(data))
+    return data
 
 def transform_data(data) -> pd.DataFrame:
-    try:
-        df = pd.DataFrame(data["daily"])
-        df.columns = [
-                    "date", "weather_code", "max_temperature", "min_temperature", 
-                    "max_apparent_temperature", "min_apparent_temperature", "rain_sum", "snowfall_sum"
-                    ]
+    df = pd.DataFrame(data["daily"])
+    df.columns = [
+                "date", "weather_code", "max_temperature", "min_temperature", 
+                "max_apparent_temperature", "min_apparent_temperature", "rain_sum", "snowfall_sum"
+                ]
 
-        df.ffill(inplace=True)
-        df.dropna(inplace=True)
-        df = df.reset_index(drop=True)
+    df.ffill(inplace=True)
+    df.dropna(inplace=True)
+    df = df.reset_index(drop=True)
 
-        df["date"] = pd.to_datetime(df["date"])
-        df["weather_code"] = df["weather_code"].astype(int)
+    df["date"] = pd.to_datetime(df["date"])
+    df["weather_code"] = df["weather_code"].astype(int)
 
-        return df
-    except Exception as e:
-        print(f"Unexpected {e}")
+    return df
 
 def main():
     data_weather = read_file('weather_past_data.json')
